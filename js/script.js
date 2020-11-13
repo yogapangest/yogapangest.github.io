b623db37f5410a813f24cead2d672f7f99cea713
yogapangest <ypangest0746@gmail.com>
2020-11-13 22:08:57 +0700

    portofilio3

--- js/script.js
+++ js/script.js
@@ -4,59 +4,75 @@ var judul2 = document.getElementsByClassName("lead");
 judul[0].classList.add("muncul-judul");
 judul2[0].classList.add("muncul-judul");
 
-
-
 window.onscroll = function () {
-    // Navigasi transparan
-    var nav = document.getElementById('nav');
-    if (window.pageYOffset > 100) {
-        nav.style.background = "black";
-        nav.style.transition = "0.4s";
-    } else {
-        nav.style.background = "transparent";
-        nav.style.transition = "0.4s";
+  // Navigasi transparan
+  var nav = document.getElementById("nav");
+  if (window.pageYOffset > 100) {
+    nav.style.background = "black";
+    nav.style.transition = "0.4s";
+  } else {
+    nav.style.background = "transparent";
+    nav.style.transition = "0.4s";
+  }
+
+  // Judul Jumbotron
+  if (window.pageYOffset > 170) {
+    judul[0].classList.remove("muncul-judul");
+    judul2[0].classList.remove("muncul-judul");
+  } else {
+    judul[0].classList.add("muncul-judul");
+    judul2[0].classList.add("muncul-judul");
+  }
+
+  // Efek Fade about
+  var profil = document.querySelectorAll(".about-content");
+  if (window.pageYOffset > 500) {
+    profil[0].classList.add("muncul");
+  } else {
+    profil[0].classList.remove("muncul");
+  }
+
+  // Efek fade beruntun education
+  var kartu = document.querySelectorAll(".content-card");
+  if (window.pageYOffset > 1200) {
+    for (let i = 0; i < kartu.length; i++) {
+      cetak(i);
     }
+  } else {
+    hapus();
+  }
 
-    // Judul Jumbotron
-    if (window.pageYOffset > 170) {
-        judul[0].classList.remove("muncul-judul");
-        judul2[0].classList.remove("muncul-judul");
-    } else {
-        judul[0].classList.add("muncul-judul");
-        judul2[0].classList.add("muncul-judul");
+  function cetak(i) {
+    setTimeout(angka, 100 * i);
+
+    function angka() {
+      kartu[i].classList.add("kart");
     }
+  }
 
-    // Efek Fade about
-    var profil = document.querySelectorAll(".about-content");
-    if (window.pageYOffset > 500) {
-        profil[0].classList.add("muncul");
-    } else {
-        profil[0].classList.remove("muncul");
+  function hapus() {
+    for (let i = 0; i < kartu.length; i++) {
+      kartu[i].classList.remove("kart");
     }
+  }
+};
 
+const container = document.querySelector(".gallery");
+const jumbo = document.querySelector(".jumbo");
+const thumbs = document.querySelectorAll(".thumb");
 
-    // Efek fade beruntun education
-    var kartu = document.querySelectorAll(".content-card");
-    if (window.pageYOffset > 1200) {
-        for (let i = 0; i < kartu.length; i++) {
-            cetak(i);
-        }
-    } else {
-        hapus();
-    }
+container.addEventListener("click", function (e) {
+  // cek apakah yang di klik adalah thumb
+  if (e.target.className == "thumb") {
+    jumbo.src = e.target.src;
+    jumbo.classList.add("fade");
+    setTimeout(function (e) {
+      jumbo.classList.remove("fade");
+    }, 500);
 
-    function cetak(i) {
-        setTimeout(angka, 100 * i);
-
-        function angka() {
-            kartu[i].classList.add("kart");
-        }
-    }
-
-    function hapus() {
-        for (let i = 0; i < kartu.length; i++) {
-            kartu[i].classList.remove("kart");
-        }
-    }
-
-}
+    thumbs.forEach(function (thumb) {
+      thumb.className = "thumb";
+    });
+    e.target.classList.add("active");
+  }
+});
--- js/script2.js
+++ js/script2.js
@@ -1,11 +1,31 @@
 for (let i = 0; i < 10; i++) {
-    cetak(i);
+  cetak(i);
 }
 
 function cetak(i) {
-    setTimeout(angka, 1000 * i);
+  setTimeout(angka, 1000 * i);
 
-    function angka() {
-        console.log(i);
-    }
+  function angka() {
+    console.log(i);
+  }
 }
+
+const container = document.querySelector(".container");
+const jumbo = document.querySelector(".jumbo");
+const thumbs = document.querySelectorAll(".thumb");
+
+container.addEventListener("click", function (e) {
+  // cek apakah yang di klik adalah thumb
+  if (e.target.className == "thumb") {
+    jumbo.src = e.target.src;
+    jumbo.classList.add("fade");
+    setTimeout(function (e) {
+      jumbo.classList.remove("fade");
+    }, 500);
+
+    thumbs.forEach(function (thumb) {
+      thumb.className = "thumb";
+    });
+    e.target.classList.add("active");
+  }
+});
